a28a241c44afa819fb87ab0cafcafb5dd2879c0b
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2023-04-11 18:52:06 +0200

    Initial Commit

---
+++ AIVoice/AIVoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIVoiceApp.swift
+//  AIVoice
+//
+//  Created by Tommy Ludwig on 11.04.23.
+//
+
+import SwiftUI
+
+@main
+struct AIVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIVoice/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AIVoice
+//
+//  Created by Tommy Ludwig on 11.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
