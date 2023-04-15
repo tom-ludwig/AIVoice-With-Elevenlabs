602ad16fcaa60f39982f8b485fe2bee417353285
Tom Ludwig <83090745+activcoding@users.noreply.github.com>
2023-04-15 15:41:01 +0200

    Added a nicer WaveformView;

--- AIVoice/AudioPlayer.swift
+++ AIVoice/AudioPlayer.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 import AVFoundation
+import DSWaveformImage
 import DSWaveformImageViews
 
 struct AudioPlayer: View {
     @State var data: Data?
     @State var audioPlayer: AVAudioPlayer?
     @State var audioFileURL: URL?
+    @State var configuration: Waveform.Configuration = Waveform.Configuration(
+        style: .striped(.init(color: .lightGray, width: 5, spacing: 2, lineCap: .round)),
+        verticalScalingFactor: 1
+    )
     var body: some View {
         VStack {
             if let audioPlayer = audioPlayer {
@@ -28,42 +33,12 @@ struct AudioPlayer: View {
                     } label: {
                         Image(systemName: "pause")
                     }.buttonStyle(.bordered).tint(.indigo)
-                    Button {
-                        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-                            return
-                        }
-                        let fileURL = documentsDirectory.appendingPathComponent("audio.mp3")
-                        do {
-                            try data?.write(to: fileURL, options: .atomic)
-                            print("Data succsesfully saved!")
-                        } catch {
-                            print("Error writing data to file: \(error)")
-                        }
-                        
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-                            self.audioFileURL = fileURL
-                        }
-                    } label: {
-                        Image(systemName: "arrow.triangle.2.circlepath")
-                    }.buttonStyle(.bordered).tint(.indigo)
                     
                     if let url = audioFileURL {
-                        WaveformView(audioURL: url)
+                        WaveformView(audioURL: url, configuration: configuration)
+                    } else {
+                        ProgressView()
                     }
-                    
-                    
-                    //                    VStack(alignment: .leading) {
-                    //                        Text("The Audio is ready")
-                    
-                    
-                    
-                    
-                    //                            .font(.system(size: 20, weight: .medium, design: .rounded))
-                    //
-                    //                        Text("Check your volume to hear something")
-                    //                            .font(.system(size: 10, weight: .medium, design: .rounded))
-                    //                        Spacer()
-                    //                    }
                 }.frame(height: 40)
             }
         }.onAppear {
@@ -79,6 +54,17 @@ struct AudioPlayer: View {
             } catch {
                 print("Error while loading audio data: \(error.localizedDescription)")
             }
+            guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+                return
+            }
+            let fileURL = documentsDirectory.appendingPathComponent("audio.mp3")
+            do {
+                try data?.write(to: fileURL, options: .atomic)
+                print("Data succsesfully saved!")
+            } catch {
+                print("Error writing data to file: \(error)")
+            }
+            self.audioFileURL = fileURL
         }
         .padding()
     }
